24459ef52183a69dfbf7e4797651e9d5702829ba
Felipe Soares <felipesilvasoares003@gmail.com>
2026-01-02 18:47:45 -0300

    fiz a logica de somatória de pontos trucados

--- src/componentes/AreaPontos.jsx
+++ src/componentes/AreaPontos.jsx
@@ -2,21 +2,51 @@ import { BsSuitClubFill as NaipePaus } from "react-icons/bs";
 import { FaHeart as NaipeCopas } from "react-icons/fa6";
 import { BsSuitSpadeFill as NaipeEspadas } from "react-icons/bs";
 import { FaDiamond as NaipeOuros } from "react-icons/fa6";
+import { useState, useEffect } from "react";
+import Contador from "../telas/Contador";
 
-export default function AreaPontos({naipe, nome, pontos}){
-    const Naipe = (
-        <NaipeCopas />
-    )
+export default function AreaPontos({naipe, nome, valorPonto, reiniciarValorTruco}){
+
+    const naipes = [
+        (<NaipeOuros/>),
+        (<NaipeEspadas/>),
+        (<NaipeCopas/>),
+        (<NaipePaus/>)
+    ]
+
+    const [selectedNaipe, setSelectedNaipe] = useState(naipes[0])
+
+    useEffect(() => {
+        const num = Math.floor(Math.random() * 4)
+        setSelectedNaipe(naipes[num])
+    }, [])
+
+    const [pontos, setPontos] = useState(0)
+
+    const mudarValorPonto = (operador) => {
+        if (operador == 1 && pontos < 12) 
+            setPontos(pontos + valorPonto)
+        else if(operador == -1 && pontos > 0)
+            setPontos(pontos - valorPonto)
+
+        reiniciarValorTruco()
+    }
+
     return (
         <>
             <div className="flex gap-4 items-center text-[32px]">
-                <span className="">{Naipe}</span>
+                <span className="">{selectedNaipe}</span>
                 <h1 className="">{nome}</h1>
             </div>
             <div className="flex flex-row justify-between items-center">
-                <button className="w-[100%] text-sm">-1</button>
+                <button className="w-[100%] h-[100%] text-sm" 
+                    onClick={() => mudarValorPonto(-1)}
+                >-{valorPonto}</button>
+
                 <span className="text-[100px]">{pontos}</span>
-                <button className="w-[100%] text-sm">+1</button>
+                <button className="w-[100%] text-sm" 
+                    onClick={() => mudarValorPonto(1)}
+                >+{valorPonto}</button>
             </div>
         </>
     )
--- src/telas/Contador.jsx
+++ src/telas/Contador.jsx
@@ -1,25 +1,65 @@
+import { useEffect, useState } from "react";
 import AreaPontos from "../componentes/AreaPontos";
 
 export default function Contador() {
     const nome1 = "Felipe"
     const nome2 = "sasa"
-    const pontos1 = 1
-    const pontos2 = 1
-    const btnTruco = "Truco"
     const naipe1 = "copas"
     const naipe2 = "paus"
+    
+    const [txtTruco, setTxtTruco] = useState("Truco!")
+    const [valorPonto, setValorPonto] = useState(1)
+
+    const trocarValorPonto = (valor) => {
+        switch(valor) {
+            case 1:
+                setTxtTruco("Pedir 6!")
+                setValorPonto(3)
+                break;
+            case 3:
+                setTxtTruco("Pedir 9!")
+                setValorPonto(6)
+                break;
+            case 6:
+                setTxtTruco("Pedir 12!")
+                setValorPonto(9)
+                break; 
+            case 9:
+                setTxtTruco("Desistir")
+                setValorPonto(12)
+                break;
+            default:
+                setTxtTruco("Truco!")    
+                setValorPonto(1)
+                break;   
+        }
+    }
 
     return(
         <section className="h-screen px-3 py-5 flex flex-col justify-between">
-            <AreaPontos naipe={naipe1} nome={nome1} pontos={pontos1} />
+            <AreaPontos naipe={naipe1} nome={nome1} 
+                valorPonto={valorPonto} 
+                reiniciarValorTruco={() => {
+                    setValorPonto(1)
+                    setTxtTruco("Truco!")
+                }} 
+            />
 
             <div className="flex flex-row justify-between items-center">
                 <hr className="w-[100%]" />
-                <button className="text-[25px] w-90 bg-black mx-1 font-medium">{btnTruco}</button>
+                <button className="text-[25px] w-90 bg-black mx-1 font-medium"
+                    onClick={() => trocarValorPonto(valorPonto)}
+                >{txtTruco}</button>
                 <hr className="w-[100%]" />
             </div>
 
-            <AreaPontos naipe={naipe2} nome={nome2} pontos={pontos2} />
+            <AreaPontos naipe={naipe2} nome={nome2} 
+                valorPonto={valorPonto} 
+                reiniciarValorTruco={() => {
+                    setValorPonto(1)
+                    setTxtTruco("Truco!")
+                }} 
+            />
 
             <span></span>
             
